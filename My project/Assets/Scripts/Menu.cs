4aa6399a09d9f8a717b563b446b17fcfeb7f8010
Mickael-95 <72389929+Mickael-95@users.noreply.github.com>
2023-01-11 09:48:52 +0100

    Update function to quit the game

--- My project/Assets/Scripts/Menu.cs
+++ My project/Assets/Scripts/Menu.cs
@@ -8,7 +8,8 @@ public class Menu : MonoBehaviour
     public void PlayGame () {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
-    public void QuitGame () {
-        Application.Quit();
+    public void QuitGame() {
+        UnityEditor.EditorApplication.isPlaying = false;
     }
+
 }
