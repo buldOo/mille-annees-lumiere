da62c48d39f752a01998367a7952aedc0c20db78
buldOo <killian.gay28@gmail.com>
2023-01-12 16:39:00 +0100

    ya zeubi

--- My project/Assets/Scripts/CardClick.cs
+++ My project/Assets/Scripts/CardClick.cs
@@ -6,7 +6,12 @@ using TMPro;
 public class CardClick : MonoBehaviour
 {
 
-    public TMP_Text cardType;
+    public TMP_Text CardType;
+    public TMP_Text CardDescr;
+
+    public GameObject playerOne;
+    public GameObject playerTwo;
+    private GameObject player;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +25,33 @@ public class CardClick : MonoBehaviour
     }
 
     public void onClick() {
-        Debug.Log(cardType.text);
+
+        if (transform.parent.tag == "playerOne")
+        {
+            player = playerOne;
+            Debug.Log("it's player one");
+        }
+        else if(transform.parent.tag == "playerTwo")
+        {
+            player = playerTwo;
+            Debug.Log("it's player two");
+        }
+
+        switch (CardType.text)
+        {
+            case "Carte Distance":
+                Debug.Log("carte distance");
+                Debug.Log(player.transform.position = new Vector2(player.transform.position.x + 10, player.transform.position.y));
+                break;
+            case "Carte Speed":
+                Debug.Log("carte speed");
+                break;
+            case "Carte Shield":
+                Debug.Log("carte shield");
+                break;
+            case "Carte Energy":
+                Debug.Log("carte energy");
+                break;
+        }
     }
 }
